18c6ae4b2111e2005c36031823e9c8884d0ab9a1
PColumba <pawel.agolab@gmail.com>
2023-10-06 12:22:24 +0200

    Add loading and error handling to issues list

--- frontend/src/routes/Issues.tsx
+++ frontend/src/routes/Issues.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
 import IssuesList from '../views/IssuesList/IssuesList';
-import { Issue } from '../model';
 import Container from "@mui/material/Container";
-import { Fab, Typography } from '@mui/material';
+import { Alert, Fab, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import useAsyncValue, { ActionState } from '../hooks/useAsyncValue';
+import { getIssues } from '../service';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const fabStyle = {
   position: 'absolute',
@@ -17,17 +18,24 @@ const AddNewFAB = () => {
   return <Fab sx={fabStyle} variant="extended" color="primary">Add new issue <AddIcon /></Fab>
 }
 const Issues: React.FC = () => {
-  const [issues, setIssues] = useState<Issue[]>([])
-  
-  useEffect(() => {
-    fetch("/issues").then(res => res.json()).then(issues => setIssues(issues))
-  }, [])
+  const [issues, _] = useAsyncValue(getIssues, [])
 
+  if(issues.type === ActionState.LOADING) {
+    return <Container maxWidth={"md"}>
+      <CircularProgress />
+    </Container>
+  }
+
+  if(issues.type === ActionState.ERROR) {
+    return <Container maxWidth={"md"}>
+      <Alert severity="error">Ups something went wrong: {typeof issues.error === "string" ? issues.error : ""}</Alert>
+    </Container>
+  }
 
   return (
     <Container maxWidth={"md"}>
       <Typography variant="h3">Issue Tracker</Typography>
-      <IssuesList issues={issues} />
+      <IssuesList issues={issues.value} />
       <AddNewFAB />
     </Container>
   );
